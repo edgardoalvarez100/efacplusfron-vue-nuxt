2c7fb521be1b7fbbf4ad9dacb157c935f19aaea0
Edgardo Alvarez <edgardo@livedesign.org>
2022-07-03 18:36:38 -0500

    “update”

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: [
-        '~/public/theme-1/vendor/fontawesome-free/css/all.min.css',
-        '~/public/theme-1/css/sb-admin-2.min.css'
+        '~/theme-1/vendor/fontawesome-free/css/all.min.css',
+        '~/theme-1/css/sb-admin-2.min.css'
     ],
 
 
